622c9af174e24c53da738c023312e4363aeb7c64
DuyVuL1 <2121051269@student.humg.edu.vn>
2023-03-13 21:56:08 +0700

    BTtuan5

---
+++ bai8.java
@@ -0,0 +1,22 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+ 
+public class bai8 {
+ 
+    public static void main(String[] args) {
+        int n, sum = 0, number;
+        float avgNumber;
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");    
+        System.out.println("Nhap so luong phan tu so nguyen: ");
+        n = scanner.nextInt();
+        for (int count = 1; count <=n; count++) {
+            System.out.println("Nhap so thu " + count + ": ");
+            number = scanner.nextInt();
+            sum += number;
+        } 
+        avgNumber = (float) sum / n;
+        System.out.println("TBC= " + decimalFormat.format(avgNumber));
+    }
+ 
+}
